8e8adb096f80a73a920d88b68c7753c815fe3d0a
unknown <infoteobalda@gmail.com>
2020-04-05 18:01:49 +0200

    maybe eh

---
+++ app_default.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello world2!"
+
+
+if __name__ == "__main__":
+    app.run()
